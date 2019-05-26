ab082492a8c3e9bc36ff6c6097a7088aa170ca3a
Alberto Hernandez <hernandezsalberto@gmail.com>
2019-05-26 03:48:01 -0500

    Commit inicial

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco'];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar( 
+        title: Text('Componentes Temporal'),  
+      ),
+      body: ListView(
+        children: _crearItems()
+        ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+          title: Text( opt ),
+        );
+
+        lista..add( tempWidget )
+             ..add( Divider() );
+    }
+
+    return lista;
+  }
+
+}
