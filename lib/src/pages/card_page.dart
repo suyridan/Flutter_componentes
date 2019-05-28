4f91d6d136e0bd976bcb18f80dc5a506fdf64621
Alberto Hernandez <hernandezsalberto@gmail.com>
2019-05-28 01:22:14 -0500

    73. Images y Fadein images

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -17,6 +17,8 @@ class CardPage extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
         ],
       ),
     );
@@ -29,7 +31,7 @@ class CardPage extends StatelessWidget {
           ListTile(
             leading: Icon( Icons.photo_album, color: Colors.blue,),
             title: Text('Titulo de tarjeta'),
-            subtitle: Text('ojawdojawodjawodjaowjdoaj aosjd lasljd asl l jdlsjdljwljdlwj sljdlsj lasjda   ljsdljsldjsdljalwfj lasjdaljfñalwjfñaljwñflj'),
+            subtitle: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae commodo tellus. Fusce tempor felis libero, ut bibendum ipsum volutpat id. Duis non mattis neque. Nunc tempor ex non efficitur consectetur. Aliquam ac fermentum velit. Integer faucibus egestas quam nec porta. Donec condimentum, justo quis viverra volutpat, urna nisl mollis tellus, at feugiat nulla enim a lorem. Quisque id velit a nisl auctor tincidunt ac in erat. Maecenas hendrerit vitae tortor a aliquam.'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
@@ -44,4 +46,28 @@ class CardPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget _cardTipo2() {
+    
+    return Card( 
+      child: Column(
+        children: <Widget>[
+
+          FadeInImage(
+            image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'),
+            placeholder: AssetImage( 'assets/jar-loading.gif' ),
+            fadeInDuration: Duration( milliseconds: 200 ),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Texto de la imagen'),
+          )
+        ],
+      ),
+    );
+  
+  }
+  
 }
