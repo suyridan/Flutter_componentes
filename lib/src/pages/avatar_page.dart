fa77f89d0c5f912b3c89977db2afc959c35da732
Alberto Hernandez <hernandezsalberto@gmail.com>
2019-05-31 01:12:32 -0500

    76. Pagina de Avatar
    Circle avatar y repaso a fadeinimage

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,12 +7,37 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina Avatar'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(1.0),
+            child: CircleAvatar(
+              radius: 26.0,
+              backgroundImage: NetworkImage('https://www.cinepremiere.com.mx/wp-content/uploads/2018/07/stanleeanimado.jpg'),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon ( Icons.add_location ),
         onPressed: (){
           Navigator.pop(context);
         },
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://i1.wp.com/noticieros.televisa.com/wp-content/uploads/2018/11/stan-lee-ap-archivo-1.png?resize=1045%2C602&quality=95&ssl=1'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
